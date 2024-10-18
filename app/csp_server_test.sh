49a0a46b206a3fc6bc70c5055e0d50cc492d1e8e
Iliya Iliev <iliyailiev3592@gmail.com>
2024-10-18 12:40:52 +0300

    app: Add CSP server test wrapper
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

---
+++ app/csp_server_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# Create emulated serial devices
+CSP_SERVER_SERIAL_DEV=/tmp/ttyUSB0
+socat -d -d -d pty,raw,echo=0,link=${CSP_SERVER_SERIAL_DEV} pty,raw,echo=0,link=/tmp/ttyUSB1 &
+sleep 1
+$1 -d ${CSP_SERVER_SERIAL_DEV} -p uart -a 10 &
+CSP_SERVER_TEST_PID=$!
+
+# Run the server for 2 second
+TIMEOUT=2
+sleep $TIMEOUT
+
+# Send sigterm to the server
+echo ${CSP_SERVER_TEST_PID}
+kill -s SIGTERM  $CSP_SERVER_TEST_PID
+pkill socat
